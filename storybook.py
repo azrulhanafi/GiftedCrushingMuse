ce1fd97b19cab5310c95bfc6164954b3db4ba656
azrulhanafi <2022616296@student.uitm.edu.my>
2024-10-01 22:43:11 +0000

    i have fix the code

--- storybook.py
+++ storybook.py
@@ -2,73 +2,84 @@ import os
 import streamlit as st
 from openai import OpenAI
 
-client = OpenAI(api_key=os.environ['OPENAI_API_KEY'])
 
-#story generator 
+client = OpenAI(api_key = os.environ ['OPENAI_API_KEY'])
+
 def story_gen(prompt):
-    system_prompt = """
-    You are a world renowned 50 years experience                  children storyteller. 
-    you will be given a concept to generate a story     suitable for ages 5-7 years old 
-    """
+  system_prompt ="""
+  You are a world renowned writer with 50 years of experience for children storyteller. 
+  You will be given a concept to generate a story suitable for ages 5-7 years old.
+  """
 
-    prompt = "friendship between ali and abu"
-
-    response = client.chat.completions.create(
-        model='gpt-3.5-turbo',
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": prompt}
-        ],
-        temperature=1.3,
-        max_tokens=100
-    )
-
-    return response.choices[0].message.content
-
-  #story cover prompt generator 
-def image_gen(prompt):
-    response = client.images.generate(
-        model = 'dall-e-2',
-        prompt = prompt,
-        size = '256x256',
-        n=1,
-      )
-
-    return response.data[0].url
-
-  #storybook method 
-def storybook(prompt):
-    story = story_gen(prompt)
-    cover = image_gen(story)
-    image = image_gen(cover)
-    st.image(image)
-    st.writer(story)
-
-st.title("Storybook Generator for kids for fun")
-st.divider()
-
-prompt = st.text_input("Enter your story concept:")
-
-if st.button("Generate Storybook"):
-    story = story_gen(prompt)
-    cover = cover_gen(story)
-    image = image_gen(cover)
-
-    st.write(image)
-    st.write(story)
-
-def cover_gen(prompt)
-    system_prompt = """"
-    you will be given a children story book. 
-    Generate a prompt for a cover art that is suitable and         shows off the story themes. The prompt will be sent to         dall-e-2"""
-
-response = client.chat.completions.create(model='gpt-3.5-turbo', messages=[{
-    "role": "system","content": system_prompt}')
-
-{"role": "user", "content": prompt}
-      ],
-      temperature=1.3,
-      max_tokens=200
+  response = client.chat.completions.create(
+      model = 'gpt-4o-mini',
+      messages = [
+          {"role": "system",
+          "content": system_prompt},
+          {"role": "user",
+          "content": prompt}
+          ],
+      temperature = 1.3,
+      max_tokens = 200
   )
 
   return response.choices[0].message.content
+
+
+#Cover prompt generator
+
+def cover_gen(prompt):
+  system_prompt ="""
+  You will be given a children's story book. 
+  Generate a prompt for cover art that is suitable and shows off the story themes.
+  The prompt will be sent to dall-e-2
+  """
+
+  response = client.chat.completions.create(
+      model = 'gpt-4o-mini',
+      messages = [
+          {"role": "system",
+          "content": system_prompt},
+          {"role": "user",
+          "content": prompt}
+          ],
+      temperature = 1.3,
+      max_tokens = 100
+  )
+
+  return response.choices[0].message.content
+
+#Image generator
+def image_gen(prompt):
+  response = client.images.generate(
+      model = 'dall-e-2',
+      prompt = prompt,
+      size = '256x256',
+      n = 1
+  )
+
+  return response.data[0].url
+
+#storybook method
+def storybook(prompt):
+  story = story_gen(prompt)
+  cover = cover_gen(story)
+  image = image_gen(cover)
+
+
+  st.write(image)
+  st.write(story)
+
+st.title("Storybook Generator for Kids for fun")
+st.divider()
+
+prompt = st.text_area("Enter your stroy concept:")
+
+if st.button("Generate Storybook"):
+    with st.spinner("Please wait..."):
+      story = story_gen(prompt)
+      cover = cover_gen(story)
+      image = image_gen(cover)
+      st.balloons()
+      st.image(image)
+      st.write(story)
